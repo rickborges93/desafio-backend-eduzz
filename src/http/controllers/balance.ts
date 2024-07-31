8313dbc553996c21d8062c2e0378155b8fae4ee9
Henrique Samensari Borges <henriquesamensari@gmail.com>
2024-07-31 12:06:21 -0300

    Balance infra, factory and repository
    Balance infra contains route and controller.
    
    Created the prisma repository to access database and factory to join all the dependences in one file.

---
+++ src/http/controllers/balance.ts
@@ -0,0 +1,19 @@
+import { makeBalanceUseCase } from '@/use-cases/factories/make-balance-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function balance(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    const userId = request.user.sub
+    const balanceUseCase = makeBalanceUseCase()
+
+    const { total } = await balanceUseCase.execute({
+      userId,
+    })
+
+    return { total }
+  } catch (err) {
+    return err
+  }
+
+  return reply.status(200).send()
+}
--- src/repositories/prisma/prisma-billings-repository.ts
+++ src/repositories/prisma/prisma-billings-repository.ts
@@ -3,6 +3,16 @@ import { BillingsRepository } from '../billings-repository'
 import { prisma } from '@/lib/prisma'
 
 export class PrismaBillingsRepository implements BillingsRepository {
+  async findManyByUserId(userId: string) {
+    const billings = await prisma.billing.findMany({
+      where: {
+        user_id: userId,
+      },
+    })
+
+    return billings
+  }
+
   async create(data: Prisma.BillingUncheckedCreateInput) {
     const billing = await prisma.billing.create({
       data,
