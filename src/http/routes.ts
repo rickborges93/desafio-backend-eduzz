d9db07cdeb108a90840ef20eec3017b87ae50841
Henrique Samensari Borges <henriquesamensari@gmail.com>
2024-07-31 09:31:12 -0300

    Register infra, factory and repository
    Register infra contains route and controller.
    
    Created the prisma repository to access database and factory to join all the dependences in one file.

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,12 @@
 import { FastifyInstance, FastifyReply } from 'fastify'
+import { register } from './controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
+  // default route to verify if it's working.
   app.get('/', (_, reply: FastifyReply) => {
     return reply.status(200).send('Welcome to BTC bank!')
   })
+
+  // system routes
+  app.post('/account', register)
 }
