5964a1843b009583b2f27bdc71dab5754f0e8671
Henrique Samensari Borges <henriquesamensari@gmail.com>
2024-07-31 11:56:00 -0300

    Error handling
    Function that makes zod error prittier and friendly. In addition to run a console error if environment is not production.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import fastify from 'fastify'
 import { appRoutes } from './http/routes'
 import fastifyJwt from '@fastify/jwt'
 import { env } from './env'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
@@ -10,3 +11,17 @@ app.register(fastifyJwt, {
 })
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
