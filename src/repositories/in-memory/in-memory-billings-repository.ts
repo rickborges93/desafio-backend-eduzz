d4c189d39301bc7c3918746a0f571fe3a1287b94
Henrique Samensari Borges <henriquesamensari@gmail.com>
2024-08-01 15:05:19 -0300

    Refactor: Extract use-case and tests
    Refactor: Extract use-case and tests, adding billings beyond btcTransactions

--- src/repositories/in-memory/in-memory-billings-repository.ts
+++ src/repositories/in-memory/in-memory-billings-repository.ts
@@ -1,9 +1,25 @@
 import { Billing, Prisma } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 import { BillingsRepository } from '../billings-repository'
+import dayjs from 'dayjs'
 
 export class InMemoryBillingsRepository implements BillingsRepository {
   public items: Billing[] = []
+
+  async findManyByUserIdAndDateRange(
+    userId: string,
+    initialDate: Date,
+    finalDate: Date,
+  ) {
+    return this.items.filter((item) => {
+      const transactionDate = dayjs(item.created_at)
+      const isOnSameDate =
+        transactionDate.isAfter(initialDate) &&
+        transactionDate.isBefore(finalDate)
+
+      return isOnSameDate && userId === item.user_id
+    })
+  }
 
   async findManyByUserId(userId: string) {
     return this.items.filter((item) => item.user_id === userId)
